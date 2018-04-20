35ca94555388e31d2e4aaf0e58cabc28b48ce357
Ben Bergen <bergen@lanl.gov>
2018-04-19 23:21:12 -0600

    Initial Check-In

---
+++ ristraio/example/utils.cc
@@ -0,0 +1,27 @@
+/*~-------------------------------------------------------------------------~~*
+ * Copyright (c) 2014 Los Alamos National Security, LLC
+ * All rights reserved.
+ *~-------------------------------------------------------------------------~~*/
+
+///
+/// \file utils.cc
+/// \authors bergen
+/// \date Initial file creation: Apr 19, 2018
+///
+
+#include <iostream>
+
+#include <ristraio/example/utils.h>
+
+namespace example {
+
+void myfunc() {
+  std::cout << "Hello World" << std::endl;
+} // myfunc
+
+} // namespace example
+
+/*~------------------------------------------------------------------------~--*
+ * Formatting options for vim.
+ * vim: set tabstop=2 shiftwidth=2 expandtab :
+ *~------------------------------------------------------------------------~--*/
